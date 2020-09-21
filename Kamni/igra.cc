76caa80b2615f0de891b17cda72f1b2c3e013920
Pechenka <Pechenka@Pechenka>
2020-09-21 16:43:16 +0500

    Добавили чуть больше

--- Kamni/igra.cc
+++ Kamni/igra.cc
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <iomanip>
-#include <cmath>
+#include <chrono>
 #include <random>
 
 
 int main(){
-	std::random_device rd;
-	std::minstd_rand rnd{rd()};
+	using clk = std::chrono::system_clock;
+
+	std::minstd_rand rnd{unsigned(clk::now().time_since_epoch().count())};
 	for (unsigned i = 0 ; i < 10; ++i){
 		std::cout << rnd() << ' ';
 		}
